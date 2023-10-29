1e953e4500aa0dcbd2d4d3f66b72c8244429e77c
bleehhhh <110563913+bleehhhh@users.noreply.github.com>
2023-10-29 10:15:48 -0500

    Update and rename index.php to LoveYou/index.php

--- LoveYou/index.php
+++ LoveYou/index.php
@@ -6,7 +6,7 @@
 
 <?php
 //If your Discord.php file is in another folder you need to change the file path!
-include("LoveYou/Discord.php");
+include("Discord.php");
 $sendembed = New Discord();
 
 //Executes the function
